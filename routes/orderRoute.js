ff1c5b3b122ef75ea47a0bce6dcd85cfc62c901c
rubiyadav <rubiflyeis@gmail.com>
2023-05-23 18:57:28 +0530

    update

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -10,19 +10,19 @@ router.post("/place-order", isAuthenticatedUser, orderController.placeOrder);
 
 router.get("/orders/me", isAuthenticatedUser, orderController.getOrders)
 
-// router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
 router.route("/order/:id").get(isAuthenticatedUser, orderController.getSingleOrder);
 
-// router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
-// router
-//   .route("/admin/orders")
-//   .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
 
 router
   .route("/admin/order/:id")
   .put(isAuthenticatedUser, authorizeRoles("admin"), orderController.updateOrder)
-//   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
