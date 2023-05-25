119d73d80830b281d947f2ee87fc6b22fc691dfd
rubiyadav <rubiflyeis@gmail.com>
2023-05-25 18:22:02 +0530

    update

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -239,7 +239,7 @@ exports.checkout = async (req, res, next) => {
         order.amountToBePaid =
             grandTotal + order.shippingPrice - order.discount;
 
-        await order.save();
+        const orderDetails = await order.save();
 
         await order.populate([
             { path: "products.product", select: { reviews: 0 } },
@@ -252,7 +252,7 @@ exports.checkout = async (req, res, next) => {
         return res.status(200).json({
             success: true,
             msg: "order created",
-            order,
+            orderDetails,
         });
     } catch (error) {
         next(error);
@@ -348,22 +348,22 @@ exports.createTransaction = async (req, res, next) => {
     }
 };
 exports.createTransactionbyAdmin = async (req, res, next) => {
-  try {
-      const order = await Order.findOne({ _id: req.params.id });
-      console.log(order, order.user);
-      let obj = {
-          user: order.user,
-          orderId: order._id,
-          date: Date.now(),
-          amount: req.body.amount,
-          paymentMode: req.body.paymentMode,
-          Status: req.body.status,
-      };
-      const data = await transaction.create(obj);
-      return res.status(200).json({ msg: "order id", data: data });
-  } catch (error) {
-      next(error);
-  }
+    try {
+        const order = await Order.findOne({ _id: req.params.id });
+        console.log(order, order.user);
+        let obj = {
+            user: order.user,
+            orderId: order._id,
+            date: Date.now(),
+            amount: req.body.amount,
+            paymentMode: req.body.paymentMode,
+            Status: req.body.status,
+        };
+        const data = await transaction.create(obj);
+        return res.status(200).json({ msg: "order id", data: data });
+    } catch (error) {
+        next(error);
+    }
 };
 exports.allTransaction = async (req, res) => {
     try {
@@ -374,10 +374,12 @@ exports.allTransaction = async (req, res) => {
     }
 };
 exports.allTransactionUser = async (req, res) => {
-  try {
-      const data = await transaction.find({user: req.user._id}).populate("user orderId");
-      res.status(200).json({ totalOrders: data.length });
-  } catch (err) {
-      res.status(400).json({ message: err.message });
-  }
+    try {
+        const data = await transaction
+            .find({ user: req.user._id })
+            .populate("user orderId");
+        res.status(200).json({ totalOrders: data.length });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 };
