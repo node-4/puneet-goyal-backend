39667e9221e1d330db6ab70d5895c5f1dfcf3255
rubiyadav <rubiflyeis@gmail.com>
2023-05-26 11:35:49 +0530

    update

--- models/AddressModel.js
+++ models/AddressModel.js
@@ -2,28 +2,42 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
 const addressSchema = new mongoose.Schema({
+  flatHouseNo: {
+    type: String,
+  },
+  floorNo: {
+    type: String,
+  },
+  TownNo: {
+    type: String,
+  },
   address: {
     type: String,
-    required: [true, "address required"],
-  },
-  city: {
-    type: String,
-    required: [true, "City is must"],
-  },
-  state: {
-    type: String,
-    required: [true, "State Must"],
-  },
-  pinCode: {
-    type: Number,
-    required: [true, "Pincode Required"],
   },
   landMark: {
     type: String,
   },
+  city: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  pinCode: {
+    type: Number,
+  },
   street: {
     type: String,
-    required: [true, "Street must"],
+  },
+  name:{
+    type: String,
+  },
+  phone:{
+    type: String,
+  },
+  saveAs:{
+    type: String,
+    enum: ["Home", "Work", "Other"],
   },
   user: {
     type: ObjectId,
